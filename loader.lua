79fa9e7b291cb9781f629496e48cdd84ce73472e
jukkree1145 <jukkree1145@gmail.com>
2026-01-11 13:28:50 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,5 @@
 -- ESP PRO v3 Loader
 
-local url = "https://raw.githubusercontent.com/USERNAME/ESP-PRO/main/main.lua"
-
+local url = "https://raw.githubusercontent.com/jukree1145/ESP-PRO-V3/main/main.lua"
 local src = game:HttpGet(url)
 loadstring(src)()
